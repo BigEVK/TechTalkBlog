837e2bfd3a87d3dfafea534b2fe348e33109b885
Eric VanKirk <eric@ericvankirk.com>
2022-08-31 18:44:22 -0500

    connection update

--- config/connection.js
+++ config/connection.js
@@ -23,7 +23,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_PW,
+        process.env.DB_PASSWORD,
         {
             host: 'localhost',
             dialect: 'mysql',
